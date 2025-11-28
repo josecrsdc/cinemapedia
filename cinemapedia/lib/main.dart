ac3358fe6c180f0cc7891ce960fcf4563ad613e3
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-28 13:27:42 +0100

    173-Implementación de la navegación y el tema en la aplicación
    - Agregada la configuración de rutas utilizando go_router.
    - Creada la pantalla principal (HomeScreen) con un marcador de posición.
    - Integrado el tema de la aplicación.
    - Actualizados los archivos pubspec para incluir dependencias necesarias.

--- cinemapedia/lib/main.dart
+++ cinemapedia/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/router/app_router.dart';
+import 'package:cinemapedia/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,13 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      theme: AppTheme().getTheme(),
     );
   }
 }
