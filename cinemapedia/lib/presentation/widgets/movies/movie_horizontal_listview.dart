3c8863c2c86c257d6b0baaffd26d050636a190d5
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-12-16 13:55:44 +0100

    197- Refactor MovieHorizontalListview to use ListView.builder for horizontal scrolling and improve slide layout

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -22,18 +23,93 @@ class MovieHorizontalListview extends StatelessWidget {
       child: Column(
         children: [
           if (title != null || subTitle != null)
-            _title(title: title, subTitle: subTitle),
+            _Title(title: title, subTitle: subTitle),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index]);
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class _title extends StatelessWidget {
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          //* Image
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(child: const CircularProgressIndicator(strokeWidth: 2)),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+
+          SizedBox(height: 5),
+
+          //* Title
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyles.titleSmall,
+            ),
+          ),
+
+          //* Rating
+          Row(
+            children: [
+              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+              const SizedBox(width: 3),
+              Text('${( movie.voteAverage.toDouble() * 10).round() / 10 }', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+              const SizedBox(width: 10),
+              Text('${movie.popularity}', style: textStyles.bodySmall,)
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
   final String? title;
   final String? subTitle;
 
-  const _title({this.title, this.subTitle});
+  const _Title({this.title, this.subTitle});
 
   @override
   Widget build(BuildContext context) {
@@ -47,12 +123,12 @@ class _title extends StatelessWidget {
           if (title != null) Text(title!, style: titleStyle),
           const Spacer(),
 
-          if (subTitle != null) 
-          FilledButton.tonal(
-            style: const ButtonStyle(visualDensity: VisualDensity.compact),
-            onPressed: () {}, 
-            child: Text(subTitle!)
-          ),
+          if (subTitle != null)
+            FilledButton.tonal(
+              style: const ButtonStyle(visualDensity: VisualDensity.compact),
+              onPressed: () {},
+              child: Text(subTitle!),
+            ),
         ],
       ),
     );
