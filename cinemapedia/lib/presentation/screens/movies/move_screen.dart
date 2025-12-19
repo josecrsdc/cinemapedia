5fb645184e86ecf1139177de763a592a206b5fa2
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-12-19 14:49:50 +0100

    210

---
+++ cinemapedia/lib/presentation/screens/movies/move_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  static const name = 'movi-screen';
+  final String movieId;
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
